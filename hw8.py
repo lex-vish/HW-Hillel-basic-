4560ebeb59850bfbe2c53b8158f8bc0aebf46290
lex-vish <113463240+lex-vish@users.noreply.github.com>
2022-11-23 00:07:00 +0200

    Add files via upload

---
+++ hw8.py
@@ -0,0 +1,23 @@
+# Візьміть гру з попереднього ДЗ ('rock scissors paper lizard spock') і модифікуйте наступним чином:
+# винесіть всі функції в окремий файл (нехай буде library.py) і імпортуцте їх звідти для роботи в основний файл
+# додайте запис статистики в файл (які фігури грали і хто переміг на кожному ході), використовуйте open.
+
+# імпорт гри "rock, Spock, paper, lizard, scissors"
+from hw8library import rpsls
+
+# введення вибору гравця серед опцій rock, Spock, paper, lizard, scissors
+player_guess = input('Enter your choice.. Options: rock, Spock, paper, lizard, scissors\n' )
+
+# виклик функції для співставлення вібору гравця та АІ, а також визначення переможця
+game_rpsls = rpsls(str(player_guess))
+
+print(game_rpsls)
+
+# запис результатів гри в файл hw8stat.txt
+with open('hw8stat.txt', 'a') as stat_file:
+    for count in range(3):
+        stat_file.write(f'{game_rpsls[count]} \n')
+
+
+
+
